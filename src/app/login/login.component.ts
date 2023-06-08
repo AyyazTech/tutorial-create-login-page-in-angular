7b9afc7b666d57d1984b078dae7dfd731d3e494a
Ayyaz Zafar <ayyazzafar@users.noreply.github.com>
2023-06-08 20:21:55 +0500

    Changes

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,7 +1,5 @@
 import { Component } from '@angular/core';
-import { AuthService } from '../auth.service';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -13,22 +11,12 @@ export class LoginComponent {
     username: ['', Validators.required],
     password: ['', Validators.required],
   });
-  constructor(
-    private authService: AuthService,
-    private fb: FormBuilder,
-    private router: Router
-  ) {}
+  constructor(private fb: FormBuilder) {}
 
   login() {
-    let user = this.authService.login(
-      this.form.value.username,
-      this.form.value.password
-    );
+    let username = this.form.value.username;
+    let password = this.form.value.password;
 
-    if (!user) {
-      alert('Invalid username or password');
-    } else {
-      this.router.navigateByUrl('/admin');
-    }
+    console.log(username, password);
   }
 }
